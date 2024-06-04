0fffb63d557522914c332715b39c37eea9394176
KiemesSix <kiemessix@gmail.com>
2024-06-05 03:32:46 +0700

    zi

---
+++ config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+    introTitle: 'Zi à!',
+    introDesc: ` Anh ước rằng, em chỉ đang giận anh, một chút hờn giận nho nhỏ mà thôi… Để rồi khi anh nói “anh xin lỗi”, em sẽ lại mỉm cười. Đừng giận anh nữa nhé!`,
+    btnIntro: 'Kệ anh',
+    title: 'Anh sai rồi huhu',
+    desc: 'Tha thứ cho anh nhé!',
+    btnYes: 'Vâng',
+    btnNo: 'Không',
+    question:'Tại sao em lại giận anh?',
+    btnReply: 'Gửi',
+    reply: 'Em tha lỗi cho anh rồi mà',
+    mess: 'Cảm ơn em đã tha thứ =)))',
+    messDesc: 'Nhắn tin lại cho anh liền nha, anh sẽ gọi.',
+    btnAccept: 'Nhắn tin',
+    messLink: 'http://instagram.com/cgphamviet' //
+}
